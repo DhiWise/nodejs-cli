9b8c3125b4ebf4ed3717d8b60dd1df275c77e058
Shubham Jasani <54108659+jasanishubh123@users.noreply.github.com>
2021-11-18 18:25:02 +0530

    Release/0.1.0 (#17)
    * feat(boilerplate app and postman): provide boilerplate app, model attributes and postman collection
    
    added one command for create boilerplate app and model with attributes and update postman
    collections
    
    * chore(release): 0.1.2
    
    * fix : change readme
    
    * change: readme.md
    
    * change : readme.md
    
    * change : readme.md
    
    * change : readme.md and remove comments
    
    Co-authored-by: shubham.jasani <shubham.jasani@coruscate.work>

--- bin/create-module.js
+++ bin/create-module.js
@@ -13,7 +13,7 @@ async function main() {
         let destinationPath = program.args.shift() || '.';
         // App name
         let projectPath = path.resolve(destinationPath);
-        projectPath = path.join(projectPath,'project_mvc_mon');
+        // projectPath = path.join(projectPath,'project_mvc_mon');
         let projectInformation = utility.getProjectTypeAndOrmType(projectPath);
         let projectType = projectInformation.projectType;
         let ormType = projectInformation.ormType;
